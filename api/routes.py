c9ce9d2401c84c650ccb02c54ec1b1ac7aabf6df
saimuthyapwar123 <saimuthyapwar123@gmail.com>
2026-03-06 13:44:09 +0530

    router and main module are modified

--- api/routes.py
+++ api/routes.py
@@ -1,1 +1,32 @@
  
+from fastapi import APIRouter
+from services.candidate_service import get_staging_candidates, send_for_ai
+
+app = APIRouter(prefix="/duplicate_candidate_detection",tags=["Duplicate Candidate Detection"])
+
+@app.get("/candidates/staging")
+def read_staging_candidates():
+    """
+    Fetch candidates from staging table
+    """
+    candidates = get_staging_candidates()
+
+    return {
+        "count": len(candidates),
+        "data": candidates
+    }
+
+
+@app.post("/candidates/{candidate_id}/verify")
+def verify_candidate(candidate_id: int):
+    """
+    Send candidate to AI verification
+    """
+
+    result = send_for_ai(candidate_id)
+
+    return result
+
+@app.get("/")
+def health_check():
+    return {"status": "API is running"}
--- main.py
+++ main.py
@@ -1,1 +1,6 @@
- 
+from fastapi import FastAPI
+from api.routes import app as api_router
+
+app = FastAPI(title= "Smart Recruitz AI")
+
+app.include_router(api_router)
