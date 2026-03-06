f7fd93fff21f29be6ac950cf4be085523c93f47e
Shiva Rama Krishna <shiva.perala@thezennialpro.com>
2026-03-06 11:51:45 +0530

    Update database connection

--- database/connection.py
+++ database/connection.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:Dinesh%40123@localhost:5432/duplicate_poc"
+DATABASE_URL = "postgresql://postgres:krishna123@localhost:5432/duplicate_poc"
 
 engine = create_engine(DATABASE_URL)
 
